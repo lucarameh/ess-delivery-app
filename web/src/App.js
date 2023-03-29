28515ddb526cffbc1797622684e27f2b06748db8
José Ronald <jronaldsilva11@gmail.com>
2023-03-29 18:21:20 -0300

    resolving conflicts

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,5 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Cardapio from './components/Cardapio';
-import PrimaryButton from './components/atoms/primary-button/PrimaryButton'
-import SecondaryButton from './components/atoms/secondary-button/SecondaryButton'
-import CloseButtonItem from './components/atoms/close-button/CloseButtonItem'
 import Sidemenu from './components/molecules/sidemenu/Sidemenu'
 
 function App() {
